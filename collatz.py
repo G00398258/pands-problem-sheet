f65c736290dcef9ff003070cc99185b6b93739ef
Gillian Kane-McLoughlin <G00398258@gmit.ie>
2021-02-18 20:47:18 +0000

    Made a second attempt at the Week 04 problem

--- collatz.py
+++ collatz.py
@@ -1,12 +1,28 @@
 # Weekly Task for Week 04
 # Author: Gillian Kane-McLoughlin
 
+listInts = []
 posint = int(input("Please enter a positive integer: "))
-while posint !=1: 
-    if posint % 2 ==0:
-        print (posint)
+
+while posint !=1:
+    if posint % 2 ==0: # if even
         posint = (posint // 2) # divide by 2, result should be an integer
-    else: 
-        print (posint)
+        listInts.append(posint)
+    else: # if not even it has to be odd
         posint = (posint * 3) + 1 # multiple x 3 first and then add 1
-print (1) # as per the example provided, we should also include 1 in the output
+        listInts.append(posint)
+
+print (listInts)
+    
+
+
+# This was my first attempt - I used the list method after watching the videos in Week 5
+#posint = int(input("Please enter a positive integer: "))
+#while posint !=1: 
+    #if posint % 2 ==0:
+        #print (posint)
+        #posint = (posint // 2) # divide by 2, result should be an integer
+    #else: 
+        #print (posint)
+        #posint = (posint * 3) + 1 # multiple x 3 first and then add 1
+#print (1) # as per the example provided, we should also include 1 in the output
