5e4fbea7712560cf5cdca1777b2b6e4d6e822304
Gillian Kane-McLoughlin <G00398258@gmit.ie>
2021-03-06 18:56:53 +0000

    Attempt at Week 07 weekly task

---
+++ es.py
@@ -0,0 +1,24 @@
+# Weekly Task for Week 07 - write a program that reads in a text file and outputs the number of e's it contains
+# The program should take the filename from an argument on the command line
+# Author: Gillian Kane-McLoughlin
+
+# See README file for references
+
+import sys # this will provide access to the command-line arguments
+
+def letterFrequency(letter): 
+    # open file in read text mode 
+    with open(sys.argv[1], 'rt') as f: # taking the file name from the command line
+        text = f.read() 
+        # declare count variable of zero to begin with
+        count = 0
+        # for loop - iterate through each character & increase count if letter is found
+        for char in text: 
+            if char == letter: 
+                count += 1
+  
+    return count 
+  
+  
+# call the function and display the count of letter 'e' (I went with lower case as per the instructions)
+print(letterFrequency('e')) 
